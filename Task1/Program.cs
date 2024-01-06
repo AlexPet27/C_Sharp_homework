c1903544014e450ed7e7e393b36e59f80e385d45
Aleksey <petrovaleksey.96@mail.ru>
2024-01-07 02:19:31 +0300

    tASK1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int a = int.Parse(Console.ReadLine()!);
+int b = 7;
+int c = 23;
+
+if (a % b %c == 0) {Console.WriteLine ("True");}
+else {Console.WriteLine ("False");}
